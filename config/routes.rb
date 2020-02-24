8750fedf5b20a88e001c929ab60ab17ade2c8d83
Antonio Diaz <antn@github.com>
2020-02-23 22:36:10 -0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root "dashboard#index"
 
+  resources :statuses, only: [:index]
+
   post "/_chatops/:chatop", controller: "chatops", action: :execute_chatop
-  get  "/_chatops" => "chatops#list"
+  get  "/_chatops", to: "chatops#list"
 
   post "/api/graphql", to: "graphql#execute"
-
-  get "/statuses", to: "statuses#index"
 end
